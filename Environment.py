6b8e24aedab74121ae0c76e2eda39c2963f7f8c3
Michael Franke <the.secret.goldfish@me.com>
2017-06-16 14:31:05 +0200

    added computation of EU, changed example lexica and implemented computation average EU over games

--- Environment.py
+++ Environment.py
@@ -31,13 +31,13 @@ def get_context():
 
 def get_lexica(stepsize = 1.0/3):
     all_lexica = []
-    o1 = [0.9,0.6]       
+    o1 = [0.3,0.7]
     h1 = [0.3,0.7]
-    c1 = [0.4,0.8]
+    c1 = [0.3,0.7]
     
-    o2 = [0.3,0.4]     
-    h2 = [0.2,0.6]
-    c2 = [0.4,0.6]
+    o2 = [0.5,0.5]
+    h2 = [0.5,0.5]
+    c2 = [0.5,0.5]
     
     features = [o1,h1,c1]
     all_lexica.append(features)
@@ -305,10 +305,10 @@ Lj = random.choice(list_of_lexica)
 printLexica()
 printGames()    
 
-semantics = semantics(g, Lj)
-printSem(semantics)
+sem = semantics(g, Lj)
+printSem(sem)
 
-literal_listener = normalise(numpy.transpose(semantics))
+literal_listener = normalise(numpy.transpose(sem))
 printLL(literal_listener)
 
 speaker_choice = normalise(numpy.exp(LAMBDA * numpy.transpose(literal_listener)))
@@ -318,10 +318,10 @@ listener_choice = normalise(numpy.transpose(speaker_choice))
 printCP("listener_choice", numpy.transpose(listener_choice))
 printLL(listener_choice)
 
-# define general function
+############
 
-def speaker_choice_FUN(game, lexicon, LAMBDA = 5):
-    sem = semantics(game, lexicon)
+def speaker_choice_FUN(g, speaker_lexicon, LAMBDA = 5):
+    sem = semantics(g, speaker_lexicon)
     literal_listener = normalise(numpy.transpose(sem))
     speaker_choice = normalise(numpy.exp(LAMBDA * numpy.transpose(literal_listener)))
     return speaker_choice
@@ -333,19 +333,21 @@ def listener_choice_FUN(game, lexicon, LAMBDA = 5):
     listener_choice = normalise(numpy.transpose(speaker_choice))
     return listener_choice
 
-def get_EU(g, speaker_type, listener_type):
-    # Michael's job
-    pass
+def get_EU(g, speaker_lexicon, listener_lexicon):
+    speaker_choice  = speaker_choice_FUN(g,speaker_lexicon)
+    listener_choice = listener_choice_FUN(g, listener_lexicon)
+    EU = numpy.sum(speaker_choice * numpy.transpose(listener_choice)) / len(g)
+    return(EU)
 
-## PSEUDO-CODE
+ngames = 1000 # how many games to sample from
+EU = numpy.zeros([len(list_of_lexica), len(list_of_lexica)]) # matrix with zeros of right size
 
-expected_utility = MATRIX(0, dim=[no. of lexica, no. of lexica])
+for s_type in range(len(list_of_lexica)):
+    for l_type in range(len(list_of_lexica)):
+        EU[s_type, l_type] += numpy.mean([get_EU(get_context(),
+                                                 list_of_lexica[s_type],
+                                                 list_of_lexica[s_type])
+                                          for g in range(ngames)])
 
-
-for speaker_type in range(len(SET_OF_ALL_LEXICA)):
-    for listener_type in range(len(SET_OF_ALL_LEXICA)):
-        for g in games:
-            expected_utility[speaker_type, listener_type] += get_EU(g,
-                                                                  SET_OF_ALL_LEXICA[speaker_type],
-                                                                  SET_OF_ALL_LEXICA[listener_type])
-expected_utility = expected_utility/len(games)
+print(EU)
+# seems like the first lexicon is doing better than the second
