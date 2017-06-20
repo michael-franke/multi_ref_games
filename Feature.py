3bf4101b1256261a2f25f21c400f0f3a91894ff6
Richard Belk <horse_edu@hotmail.com>
2017-06-20 10:04:46 +0200

    Delete Feature.py
    Unused

--- Feature.py
+++
@@ -1,6 +0,0 @@
-#FEATURE
-
-class Feature(object):
-    def __init__(self, *args):
-        self.ID = 0                 #Every feature has a unique ID for quick reference
-        self.value = 0              #value of the feature
