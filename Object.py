7bd90d76259439c59ea62c063a024c84c2c92c8a
Richard Belk <horse_edu@hotmail.com>
2017-06-20 10:05:10 +0200

    Delete Object.py
    Unused

--- Object.py
+++
@@ -1,6 +0,0 @@
-#OBJECT
-
-class Obj(object):
-    def __init__(self, *args):
-        self.ID = 0             #every object has a unique ID for referencing
-        self.features = []      #list of features the object has
